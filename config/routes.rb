863df89da8843398e38215bb87b1337852e0ee86
Abby <y.abby.0313@gmail.com>
2019-01-06 03:18:22 +0000

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'users/index'
 
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'toppages/index'
+
+  root to: 'toppages#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
   resources :tasks
-  end
+  resources :users, only: [:new, :create]
+end
