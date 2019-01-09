dcc00ecddc13626f889c17f40764bd05f9e1ab80
Abby <y.abby.0313@gmail.com>
2019-01-09 15:18:48 +0000

    20190109

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
     if login(email, password)
       flash[:success] = 'ログインに成功しました。'
       @user = User.find_by(email: email)
-      redirect_to @task
+#      redirect_to @task
+       redirect_to root_url
     else
       flash.now[:danger] = 'ログインに失敗しました。'
       render 'new'
