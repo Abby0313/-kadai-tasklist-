876fd8d2cbf71f2c1c1cd99a16a1f6898a076829
Abby <y.abby.0313@gmail.com>
2019-01-12 14:00:28 +0000

    lesson12-1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user
+  #  before_action :correct_user, only: [:index, :show,:create,:edit,:update,:destroy]
 
   def index
-    @tasks = Task.all.page(params[:page])
+    @tasks = current_user.tasks.all.page(params[:page])
+#    @tasks = Task.all.page(params[:page])
   end
 
   def show
@@ -9,7 +13,8 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.new
+#    @task = Task.new
   end
 
   def create
@@ -53,11 +58,20 @@ class TasksController < ApplicationController
   private
   
     def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
+#    @task = Task.find(params[:id])
     end
 
     # Strong Parameter
     def tasks_params
       params.require(:task).permit(:content, :status)
     end
+    
+    def correct_user
+    @micropost = current_user.tasks.find_by(id: params[:id])
+    unless @task
+#      redirect_to root_url
+    end
+  end
+
 end
